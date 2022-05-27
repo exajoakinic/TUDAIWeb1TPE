cbba8afedc366d06f3643cc57e81c800f5ee0c0d
Joaquin Iglesias Coqueti <jiglesiascoqueti@alumnos.exa.unicen.edu.ar>
2022-05-27 16:07:24 -0300

    se agregó formulario a menú para agregar filas a la tabla en tiempo de ejecución

--- js/script_index.js
+++ js/script_index.js
@@ -35,4 +35,3 @@ function validarFormulario() {
         randomizeCaptcha();
         }    
 }
-
--- js/script_menu.js
+++ js/script_menu.js
@@ -1,112 +1,129 @@
 let menu = [
-    {   "plato" :"Lasagna",
-        "precio":"350",
-        "origen":"Italia",
-        "apto"  :{  "vegetarianos":false,
-                    "celiacos":false}
+    {
+        "nombre": "Lasagna",
+        "precio": "350",
+        "origen": "Italia",
+        "apto_veg": false,
+        "apto_celiacos": false
     },
-    {   "plato" :"Pizza Napolitana",
-        "precio":"370",
-        "origen":"Italia",
-        "apto"  :{  "vegetarianos":true, 
-                    "celiacos":false}
+    {
+        "nombre": "Pizza Napolitana",
+        "precio": "370",
+        "origen": "Italia",
+        "apto_veg": true,
+        "apto_celiacos": false
     },
-    {   "plato" :"Calzone de Verdura",
-        "precio":"280",
-        "origen":"Italia",
-        "apto"  :{  "vegetarianos":true, 
-                    "celiacos":false}
+    {
+        "nombre": "Calzone de Verdura",
+        "precio": "280",
+        "origen": "Italia",
+        "apto_veg": true,
+        "apto_celiacos": false
     },
-    {   "plato" :"Bruschetta",
-        "precio":"330",
-        "origen":"Italia",
-        "apto"  :{  "vegetarianos":true, 
-                    "celiacos":false}
+    {
+        "nombre": "Bruschetta",
+        "precio": "330",
+        "origen": "Italia",
+        "apto_veg": true,
+        "apto_celiacos": false
     },
-    {   "plato" :"Ratatouille",
-        "precio":"500",
-        "origen":"Italia",
-        "apto"  :{  "vegetarianos":true, 
-                    "celiacos":true}
+    {
+        "nombre": "Ratatouille",
+        "precio": "500",
+        "origen": "Italia",
+        "apto_veg": true,
+        "apto_celiacos": true
     },
-    {   "plato" :"Briami Grigo",
-        "precio":"540",
-        "origen":"Grecia",
-        "apto"  :{  "vegetarianos":true, 
-                    "celiacos":false}
+    {
+        "nombre": "Briami Grigo",
+        "precio": "540",
+        "origen": "Grecia",
+        "apto_veg": true,
+        "apto_celiacos": false
     },
-    {   "plato" :"Musaka",
-        "precio":"290",
-        "origen":"Grecia",
-        "apto"  :{  "vegetarianos":false, 
-                    "celiacos":false}
+    {
+        "nombre": "Musaka",
+        "precio": "290",
+        "origen": "Grecia",
+        "apto_veg": false,
+        "apto_celiacos": false
     },
-    {   "plato" :"Risotto",
-        "precio":"360",
-        "origen":"Grecia",
-        "apto"  :{  "vegetarianos":true, 
-                    "celiacos":false}
+    {
+        "nombre": "Risotto",
+        "precio": "360",
+        "origen": "Grecia",
+        "apto_veg": true,
+        "apto_celiacos": false
     },
-    {   "plato" :"Albondigas Con Salsa de Limón",
-        "precio":"450",
-        "origen":"Grecia",
-        "apto"  :{  "vegetarianos":false, 
-                    "celiacos":false}
+    {
+        "nombre": "Albondigas Con Salsa de Limón",
+        "precio": "450",
+        "origen": "Grecia",
+        "apto_veg": false,
+        "apto_celiacos": false
     },
-    {   "plato" :"Ensalada César Sin Gluten",
-        "precio":"520",
-        "origen":"Grecia",
-        "apto"  :{  "vegetarianos":false, 
-                    "celiacos":true}
+    {
+        "nombre": "Ensalada César Sin Gluten",
+        "precio": "520",
+        "origen": "Grecia",
+        "apto_veg": false,
+        "apto_celiacos": true
     },
-    {   "plato" :"Gazpacho Andaluz Tradicional",
-        "precio":"330",
-        "origen":"España",
-        "apto"  :{  "vegetarianos":true, 
-                    "celiacos":false}
+    {
+        "nombre": "Gazpacho Andaluz Tradicional",
+        "precio": "330",
+        "origen": "España",
+        "apto_veg": true,
+        "apto_celiacos": false
     },
-    {   "plato" :"Falafel",
-        "precio":"390",
-        "origen":"India",
-        "apto"  :{  "vegetarianos":true, 
-                    "celiacos":false}
+    {
+        "nombre": "Falafel",
+        "precio": "390",
+        "origen": "India",
+        "apto_veg": true,
+        "apto_celiacos": false
     },
-    {   "plato" :"Bacalao Dorado",
-        "precio":"600",
-        "origen":"Portugal",
-        "apto"  :{  "vegetarianos":false, 
-                    "celiacos":true}
+    {
+        "nombre": "Bacalao Dorado",
+        "precio": "600",
+        "origen": "Portugal",
+        "apto_veg": false,
+        "apto_celiacos": true
     }
 ];
 
 let tableMenuTbody = document.querySelector("#table-menu tbody");
 inicializarTabla()
-
+// FUNCIONES INICIALIZACIÓN Y LLENADO DE TABLA DESDE JSON
 function inicializarTabla() {
     let filas = "";
     for (elem of menu) {
-        console.table(elem);
         filas += htmlMenuRow(elem);
     }
     tableMenuTbody.innerHTML = filas;
 }
 function htmlMenuRow(plato) {
     //Vamos almacenando en lo que va a devolver la función
-    let r="<tr>";
-    r+="<td>" + plato.plato + "</td>";
-    r+="<td>$" + plato.precio + "</td>";
-    r+="<td>" + htmlCampoOrigen(plato.origen) + "</td>";
-    r+="<td>" + htmlCampoApto(plato.apto) + "</td>";
-    
+    let r = "<tr>";
+    r += htmlMenuRowWithoutTR(plato);
+    r += "</td>"
     //Devuelvemos el valor de r, con el html de la fila
     return r;
+}
+function htmlMenuRowWithoutTR(plato) {
+    let r 
+    r = "<td>" + plato.nombre + "</td>";
+    r += "<td>$" + plato.precio + "</td>";
+    r += "<td>" + htmlCampoOrigen(plato.origen) + "</td>";
+    r += "<td>" + htmlCampoApto(plato);
+    return r
 }
 function htmlCampoOrigen(origen) {
     //origen debe estar en minúsicolas para que tome correctamente la clase:
     pais = origen.toLowerCase();
-    
+
     //Vamos almacenando en lo que va a devolver la función
-    let r="<span class='origen origen_" + pais + "' title='" + origen + "'>";
+    let r = "<span class='origen origen_" + pais + "' title='" + origen + "'>";
     r += origen;
     r += "</span>";
 
@@ -114,20 +131,41 @@ function htmlCampoOrigen(origen) {
     return r;
 
 }
-function htmlCampoApto(apto) {
+function htmlCampoApto(plato) {
     //Vamos almacenando en lo que va a devolver la función
-    let r="";
-    if (apto.vegetarianos) {
+    let r = "";
+    if (plato.apto_veg) {
         r += "<span class='apto apto_veg' title='Vegetarianos'>";
         r += "Vegetarianos";
         r += "</span>";
     }
 
-    if (apto.celiacos) {
+    if (plato.apto_celiacos) {
         r += "<span class='apto apto_celiacos' title='Celiacos'>";
         r += "Celiacos";
         r += "</span>";
     }
 
     return r;
+}
+// FIN FUNCIONES INICIALIZACIÓN Y LLENADO DE TABLA DESDE JSON
+
+let formulario = document.querySelector("#form_menu");
+formulario.addEventListener("submit", function (event) {
+    debugger
+    event.preventDefault();
+    let plato = {
+        "nombre": formulario.nombre.value,
+        "precio": formulario.precio.value,
+        "origen": formulario.origen.value,
+        "apto_veg": formulario.apto_veg.checked,
+        "apto_celiacos": formulario.apto_celiacos.checked
+    };
+    addToTable(plato);
+});
+
+function addToTable(plato) {
+    fila = document.createElement("tr");
+    fila.innerHTML  = htmlMenuRowWithoutTR(plato)
+    tableMenuTbody.appendChild(fila);
 }
